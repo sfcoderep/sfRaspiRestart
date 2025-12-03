565f31df60c6cacaa6ff826a45c2ad68a5a423f1
Mason <dismal.ict@outlook.com>
2025-12-03 14:44:55 -0600

    v9

--- datawall-restart.sh
+++ datawall-restart.sh
@@ -64,22 +64,26 @@ click_dashboard_button() {
     log_message "Clicking dashboard button ${DASHBOARD_SELECTION}..."
     
     # Click based on button position
+    # Buttons appear to be centered on page, so we adjust for that
     case $DASHBOARD_SELECTION in
         1)
-            # Top button - upper third of screen
-            CLICK_Y=$((SCREEN_HEIGHT / 4))
+            # Top button - "Single CNC Dashboard"
+            # Click at 30% from top (above center)
+            CLICK_Y=$((SCREEN_HEIGHT * 30 / 100))
             xdotool mousemove $CENTER_X $CLICK_Y click 1
             log_message "Clicked top dashboard button at ($CENTER_X, $CLICK_Y)"
             ;;
         2)
-            # Middle button - center of screen
+            # Middle button - "Horseshoe Cell Dashboard"
+            # Click at exact center
             CLICK_Y=$((SCREEN_HEIGHT / 2))
             xdotool mousemove $CENTER_X $CLICK_Y click 1
             log_message "Clicked middle dashboard button at ($CENTER_X, $CLICK_Y)"
             ;;
         3)
-            # Bottom button - lower third of screen
-            CLICK_Y=$((SCREEN_HEIGHT * 3 / 4))
+            # Bottom button - "UMC350-HD Cell"
+            # Click at 70% from top (below center)
+            CLICK_Y=$((SCREEN_HEIGHT * 70 / 100))
             xdotool mousemove $CENTER_X $CLICK_Y click 1
             log_message "Clicked bottom dashboard button at ($CENTER_X, $CLICK_Y)"
             ;;
